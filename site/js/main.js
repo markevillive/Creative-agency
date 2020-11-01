f9022799544d6b1f8ea2bac36b2a3fda4d7bab97
Marc Shreder <mark_evillive@hotmail.com>
2020-11-02 06:00:53 +0700

    Lesson 24.1.0

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,21 @@
-$(function (){
+$(function(){
+
     baguetteBox.run('.gallery');
-    }
-);
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
 function onYouTubeIframeAPIReady() {
     var iStatus;
 
