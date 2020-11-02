e7a6870bedb0355480ebbfb254b4746741070466
Marc Shreder <mark_evillive@hotmail.com>
2020-11-02 20:18:02 +0700

    Lesson 25.2.0

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,9 @@
 $(function(){
+    $(window).on('load', function(){
+        $('.preloader').delay(500).fadeOut('slow', function(){
+            $(this).attr('style', 'display: none !important');
+        });
+    });
 
     baguetteBox.run('.gallery');
 
